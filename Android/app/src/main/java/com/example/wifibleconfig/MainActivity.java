2b497209303bdea27eb3c97554496ca326c7da64
btframework <44606899+btframework@users.noreply.github.com>
2023-02-28 22:36:05 +0300

    refactoring

--- Android/app/src/main/java/com/example/wifibleconfig/MainActivity.java
+++ Android/app/src/main/java/com/example/wifibleconfig/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.wifibleconfig;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothGattCallback;
 import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothGattDescriptor;
 import android.bluetooth.BluetoothGattService;
+import android.bluetooth.BluetoothManager;
 import android.bluetooth.BluetoothProfile;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
@@ -14,6 +18,9 @@ import android.bluetooth.le.ScanFilter;
 import android.bluetooth.le.ScanResult;
 import android.bluetooth.le.ScanSettings;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.ParcelUuid;
 import android.view.View;
@@ -21,13 +28,25 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.jetbrains.annotations.NotNull;
+
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.UUID;
 
-public class MainActivity extends PermissionsChecker {
+public class MainActivity extends AppCompatActivity {
+    //region Constants
+    private final static int ENABLE_BLUETOOTH_REQUEST_CODE = 1;
+    private final static int RUNTIME_PERMISSION_REQUEST_CODE = 2;
+    //endregion
 
     //region Bluetooth objects
+    protected BluetoothManager bluetoothManager;
+    protected BluetoothAdapter bluetoothAdapter;
     private BluetoothLeScanner bluetoothLeScanner;
     private BluetoothGatt gattClient;
     private BluetoothGattCharacteristic wifiCharacteristic;
@@ -44,6 +63,93 @@ public class MainActivity extends PermissionsChecker {
     //endregion
 
     private byte currentCommand;
+
+    //region Permissions control
+    private boolean hasPermission(String permissionType) {
+        return (checkSelfPermission(permissionType) == PackageManager.PERMISSION_GRANTED);
+    }
+
+    @SuppressLint("MissingPermission")
+    private void promptEnableBluetooth() {
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, ENABLE_BLUETOOTH_REQUEST_CODE);
+        }
+    }
+
+    private void requestBluetoothPermissions() {
+        showRequestPermissionDialog(R.string.bluetooth_dlg_title, R.string.bluetooth_dlg_text,
+                new String[]{
+                        android.Manifest.permission.BLUETOOTH_SCAN,
+                        android.Manifest.permission.BLUETOOTH_CONNECT
+                });
+    }
+
+    private void requestLocationPermission() {
+        showRequestPermissionDialog(R.string.location_dlg_title, R.string.location_dlg_text,
+                new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION});
+    }
+
+    private void showRequestPermissionDialog(int titleId, int textId, String[] permissions) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(getResources().getString(titleId));
+        builder.setMessage(getResources().getString(textId));
+        builder.setPositiveButton(android.R.string.ok, null);
+        builder.setOnDismissListener(dialog -> requestPermissions(permissions, RUNTIME_PERMISSION_REQUEST_CODE));
+        builder.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == ENABLE_BLUETOOTH_REQUEST_CODE) {
+            if (resultCode != Activity.RESULT_OK)
+                promptEnableBluetooth();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (!bluetoothAdapter.isEnabled()) {
+            promptEnableBluetooth();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NotNull String[] permissions,
+                                           @NotNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case RUNTIME_PERMISSION_REQUEST_CODE:
+                boolean containsDenial = Arrays.stream(grantResults).anyMatch(i -> i == PackageManager.PERMISSION_DENIED);
+                if (containsDenial)
+                    requestRelevantRuntimePermissions();
+                break;
+        }
+    }
+
+    private boolean hasRequiredRuntimePermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            return (hasPermission(android.Manifest.permission.BLUETOOTH_SCAN) &&
+                    hasPermission(android.Manifest.permission.BLUETOOTH_CONNECT));
+        }
+
+        return hasPermission(Manifest.permission.ACCESS_FINE_LOCATION);
+    }
+
+    private void requestRelevantRuntimePermissions() {
+        if (!hasRequiredRuntimePermissions()) {
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S)
+                requestLocationPermission();
+            else
+                requestBluetoothPermissions();
+        }
+    }
+    //endregion
 
     //region Bluetooth callbacks
     private BluetoothGattCallback gattCallback = new BluetoothGattCallback() {
@@ -416,6 +522,8 @@ public class MainActivity extends PermissionsChecker {
         wifiStatusLabel = (TextView)findViewById(R.id.WiFiStatusLabel);
         wifiStatusLabel.setVisibility(View.INVISIBLE);
 
+        bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        bluetoothAdapter = bluetoothManager.getAdapter();
         bluetoothLeScanner = null;
         gattClient = null;
         wifiCharacteristic = null;
